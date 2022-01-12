fc9d27fb1a217715d23a060abd084d712670d758
Guilherme Felipe <guilhermeempresa158@gmail.com>
2022-01-12 09:23:24 -0300

    first commit

--- src/teste.py
+++ src/teste.py
@@ -3,6 +3,7 @@ from os import system as t
 t("rm .git*")
 t("git init")
 t("git add .")
+t("git remote set-url origin https://github.com/guilipeh-git/-whatsapp_bot_gui.git")
 t(r'git commit -m "first commit" ')
 
 t("git push -u origin master")
