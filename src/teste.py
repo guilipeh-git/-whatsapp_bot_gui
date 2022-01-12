891c4f20a4ec9e97d6f0adf4b0a9f69c25eb0583
Guilherme Felipe <guilhermeempresa158@gmail.com>
2022-01-12 09:04:16 -0300

    firts commit

--- src/teste.py
+++ src/teste.py
@@ -1,7 +1,8 @@
 from os import system as t
 
-t("rd .git /S/Q")
+t("rm .git*")
 t("git init")
 t("git add .")
-t("git commit -m "+"first commit")
+t(r'git commit -m "first commit" ')
+
 t("git push -u origin master")
