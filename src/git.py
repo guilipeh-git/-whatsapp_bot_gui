ccf58460ff36784f005226fde6f2b14446763edb
guilipeh-git <80486962+guilipeh-git@users.noreply.github.com>
2022-01-21 09:16:20 -0300

    Update git.py

--- src/git.py
+++ src/git.py
@@ -3,7 +3,7 @@
 t("rd .git*")
 t("git init")
 t("git add .")
-t("git remote set-url origin https://github.com/guilipeh-git/-whatsapp_bot_gui.git")
+t("git remote set-url origin https://github.com/guilipeh-git/-whatsapp_bot_gui.git")         # se for o primeiro commit  tira o set-url e coloca add 
 t(r'git commit -m "first commit" ')
 
 t("git push -u origin master")"""
